645b1435789b4e597b8dcebc308d92616f117167
AdityaGoloka <adityatrivedi@golokait.com>
2024-02-06 16:25:26 +0530

    Blogs, Books Section Card UI Changed

--- Client/src/components/Books/Books.jsx
+++ Client/src/components/Books/Books.jsx
@@ -1,6 +1,6 @@
 import { useRef, useEffect, useState } from "react";
 import "./Books.scss";
-import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+import { motion, useAnimation, useScroll, useSpring, useTransform } from "framer-motion";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
 import { Navigation } from "swiper/modules";
@@ -80,29 +80,80 @@ const Books = () => {
     damping: 30,
   });
 
+  const controls = useAnimation();
+
+  const formatDate = (createdAt) => {
+    const date = new Date(createdAt);
+    return date.toLocaleDateString();
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const windowHeight = window.innerHeight;
+
+      if (scrollY > windowHeight / 2) {
+        controls.start({ opacity: 1 });
+      } else {
+        controls.start({ opacity: 0 });
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [controls]);
+
   return (
-    <div className="portfolio" ref={ref}>
-      <div className="progress">
-        <h1>Books</h1>
-        <motion.div style={{ scaleX }} className="progressBar"></motion.div>
-      </div>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <Swiper
-          navigation
-          className="swiper-container"
-          slidesPerView={1}
-          spaceBetween={20}
-        >
+    <motion.section
+      className="text-gray-600 body-font"
+      initial={{ opacity: 0 }}
+      animate={controls}
+    >
+      <div className="container px-5 py-10 mx-auto">
+        <div className="flex flex-wrap w-full mb-16 justify-center">
+          <div className="lg:w-1/2 w-full mb-6 lg:mb-0 text-center">
+            <motion.h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">
+              Explore My Books
+            </motion.h1>
+            <div className="h-1 w-full bg-indigo-500 rounded"></div>
+          </div>
+        </div>
+        <div className="flex flex-wrap -m-4">
           {items.map((item) => (
-            <SwiperSlide key={item._id}>
-              <Single item={item} />
-            </SwiperSlide>
+            <motion.div
+              key={item._id}
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: 0.2 }}
+              className="xl:w-1/3 md:w-1/2 p-4"
+            >
+              <div className="bg-gray-100 p-6 rounded-lg h-[23rem] w-[27rem]">
+                <img
+                  className="h-48 w-full object-contain object-top mb-6 rounded border-2 border-red-600"
+                  src={item.bookThumbnail}
+                  alt={item.bookName}
+                />
+                <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
+                  {formatDate(item.createdAt)}
+                </h3>
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  {item.title}
+                </h2>
+                <p className="text-base line-clamp-3">{item.bookDetail}</p>
+              </div>
+            </motion.div>
           ))}
-        </Swiper>
-      )}
-    </div>
+        </div>
+        <div className="flex justify-center items-center ">
+          <a href="/blogs">
+            <button className="text-white border border-blue-400 bg-blue-400 rounded-lg px-8 py-4 mt-8">Read More</button>
+          </a>
+        </div>
+      </div>
+    </motion.section >
   );
 };
 
--- Client/src/pages/components/blogs/blogs.jsx
+++ Client/src/pages/components/blogs/blogs.jsx
@@ -1,74 +1,15 @@
-import { useRef, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { motion, useAnimation } from "framer-motion";
 import "./blogs.scss";
-import { motion, useScroll, useSpring, useTransform } from "framer-motion";
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore from "swiper";
-import { Navigation } from "swiper/modules";
-import { useNavigate } from "react-router-dom"; // Import navigate from react-router-dom
-
-SwiperCore.use([Navigation]);
-
-const Single = ({ item }) => {
-  const ref = useRef();
-  const navigate = useNavigate();
-
-  const { scrollYProgress } = useScroll({
-    target: ref,
-  });
-
-  const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
-
-  const truncatedBlogDetail =
-    item.content.length > 20
-      ? `${item.content.split(" ").slice(0, 50).join(" ")} ...`
-      : item.content;
-
-  const handleViewMore = (id) => {
-    // Navigate to the article detail page
-    navigate(`/article/${id}`);
-  };
-
-  return (
-    <section>
-      <div className="container mx-10 py-3 sm:mx-0 sm:my-0">
-        <div className="wrapper">
-          <div className="imageContainer" ref={ref}>
-            <img
-              src={item.thumbnail}
-              className="sm:w-[80%] sm:mx-auto lg:w-full mx-10"
-              alt=""
-            />
-          </div>
-          <motion.div className="textContainer sm:-my-10" style={{ y }}>
-            <h2>{item.title}</h2>
-            <p>{truncatedBlogDetail}</p>
-            <div>
-              {/* <select>
-                <option defaultValue>Country</option>
-                <option value={item.country}>{item.country}</option>
-              </select> */}
-              <a href={item.bookLink} target="_blank" rel="noopener noreferrer">
-                <button onClick={() => handleViewMore(item._id)}>Buy</button>
-              </a>
-            </div>
-          </motion.div>
-        </div>
-      </div>
-    </section>
-  );
-};
 
 const Blogs = () => {
-  const ref = useRef();
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(
-          "http://localhost:5000/api/articles/getArticles"
-        );
+        const response = await fetch("http://localhost:5000/api/articles/getArticles");
         const data = await response.json();
         setItems(data.data);
         console.log(items);
@@ -82,39 +23,80 @@ const Blogs = () => {
     fetchData();
   }, []);
 
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["end end", "start start"],
-  });
+  const formatDate = (createdAt) => {
+    const date = new Date(createdAt);
+    return date.toLocaleDateString();
+  };
 
-  const scaleX = useSpring(scrollYProgress, {
-    stiffness: 100,
-    damping: 10,
-  });
+  const controls = useAnimation();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const windowHeight = window.innerHeight;
+
+      if (scrollY > windowHeight / 2) {
+        controls.start({ opacity: 1 });
+      } else {
+        controls.start({ opacity: 0 });
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [controls]);
 
   return (
-    <div className="portfolio overflow-x-hidden " ref={ref}>
-      <div className="progress">
-        <h1>Blogs</h1>
-        <motion.div style={{ scaleX }} className="progressBar"></motion.div>
-      </div>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <Swiper
-          navigation
-          className="swiper-container"
-          slidesPerView={1}
-          spaceBetween={20}
-        >
-          {items.map((item) => (
-            <SwiperSlide key={item._id}>
-              <Single item={item} />
-            </SwiperSlide>
+    <motion.section
+      className="text-gray-600 body-font"
+      initial={{ opacity: 0 }}
+      animate={controls}
+    >
+      <div className="container px-5 py-10 mx-auto">
+        <div className="flex flex-wrap w-full mb-16 justify-center">
+          <div className="lg:w-1/2 w-full mb-6 lg:mb-0 text-center">
+            <motion.h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">
+              Explore My Blogs
+            </motion.h1>
+            <div className="h-1 w-full bg-indigo-500 rounded"></div>
+          </div>
+        </div>
+        <div className="flex flex-wrap -m-4">
+          {items.slice(0, 6).map((item) => (
+            <motion.div
+              key={item._id}
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: 0.2 }}
+              className="xl:w-1/3 md:w-1/2 p-4"
+            >
+              <div className="bg-gray-100 p-6 rounded-lg h-[18rem] w-[27rem]">
+                <img
+                  className="h-48 w-full object-contain object-center mb-6 rounded border-2 border-red-600"
+                  src={item.thumbnail}
+                  alt={item.title}
+                />
+                <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
+                  {formatDate(item.createdAt)}
+                </h3>
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  {item.title}
+                </h2>
+                <p className="text-base leading-5">{item.content}</p>
+              </div>
+            </motion.div>
           ))}
-        </Swiper>
-      )}
-    </div>
+        </div>
+        <div className="flex justify-center items-center ">
+          <a href="/blogs">
+            <button className="text-white border border-blue-400 bg-blue-400 rounded-lg px-8 py-4 mt-8">Read More</button>
+          </a>
+        </div>
+      </div>
+    </motion.section >
   );
 };
 
