f01784d02ecb131b8a22e847fd4d9528e743dbbb
AdityaGoloka <adityatrivedi@golokait.com>
2024-02-08 19:49:21 +0530

    Updates on Various Sections

--- Client/src/components/Footer/Footer.jsx
+++ Client/src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Social from "../../pages/components/Social";
 
 const Footer = () => {
   const date = new Date();
@@ -6,42 +7,19 @@ const Footer = () => {
     <div>
       <footer className=" bg-blueGray-200 pt-8  bottom-0">
         <div className="container mx-auto px-4">
-          <div className="flex flex-wrap text-left lg:text-left">
-            <div className="w-full lg:w-6/12 px-4">
-              <h4 className="text-3xl fonat-semibold text-blueGray-700">
+          <div className="flex flex-wrap text-left lg:text-left justify-center items-center">
+            <div className="w-full lg:w-6/12 px-4 ">
+              <h4 className="text-5xl fonat-semibold text-blueGray-700 text-center">
                 Let us keep in touch!
               </h4>
-              <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
+              <h5 className="text-2xl mt-0 mb-2 text-blueGray-600 text-center">
                 Find us on any of these platforms, we respond 1-2 business days.
               </h5>
-              <div className="mt-6 lg:mb-0 mb-6">
-                <button
-                  className="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
-                  type="button"
-                >
-                  <i className="fab fa-twitter"></i>
-                </button>
-                <button
-                  className="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
-                  type="button"
-                >
-                  <i className="fab fa-facebook-square"></i>
-                </button>
-                <button
-                  className="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
-                  type="button"
-                >
-                  <i className="fab fa-dribbble"></i>
-                </button>
-                <button
-                  className="bg-white text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
-                  type="button"
-                >
-                  <i className="fab fa-github"></i>
-                </button>
+              <div className="mt-6 lg:mb-0 mb-6 flex justify-center items-center ">
+                <Social />
               </div>
             </div>
-            <div className="w-full lg:w-6/12 px-4">
+            {/* <div className="w-full lg:w-6/12 px-4">
               <div className="flex flex-wrap items-top mb-6">
                 <div className="w-full lg:w-4/12 px-4 ml-auto">
                   <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
@@ -122,18 +100,14 @@ const Footer = () => {
                   </ul>
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
           <hr className="my-6 border-blueGray-300" />
           <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-4/12 px-4 mx-auto text-center">
-              <div className="text-sm text-blueGray-500 font-semibold py-1">
+              <div className="text-md text-blueGray-500 font-semibold py-1">
                 Copyright ©{" "}
                 <span id="get-current-year">{date.getFullYear()}</span>
-                {/* <a href="https://www.creative-tim.com/product/notus-js" className="text-blueGray-500 hover:text-gray-800" target="_blank" rel="noreferrer"> Notus JS by
-                                </a>
-                                <a href="https://www.creative-tim.com?ref=njs-profile" className="text-blueGray-500 hover:text-blueGray-800">Creative Tim
-                                </a>. */}
               </div>
             </div>
           </div>
--- Client/src/pages/Videos/VideoCard.jsx
+++ Client/src/pages/Videos/VideoCard.jsx
@@ -21,10 +21,10 @@ const VideoCard = ({ videoName, video_url }) => {
 
                 <div className="flex w flex-col p-4 leading-normal max-w-5xl">
                     <h5 className='mb-2 text-2xl font-bold tracking-tight text-white max-w-5xl'>{videoName}</h5>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 w-3xl">
+                    {/* <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 w-3xl">
                         Here are the biggest enterprise technology acquisitions of 2021 so
                         far, in reverse chronological order.
-                    </p>
+                    </p> */}
                 </div>
             </div>
         </div>
