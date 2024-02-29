00d5caa40516623d85048772a1c0aff68c51b626
Zaid1681 <zaidkhan1681@gmail.com>
2024-02-29 23:11:20 +0530

    BASE_URL Changed

--- Client/src/api.js
+++ Client/src/api.js
@@ -1,2 +1,2 @@
-export const OLD_BASE_URL = "http://localhost:5000/api";
-export const BASE_URL = "http://localhost:8080";
+// export const OLD_BASE_URL = "http://localhost:5000/api";
+export const BASE_URL = "http://15.207.44.110:8080/server-0.0.1-SNAPSHOT";
